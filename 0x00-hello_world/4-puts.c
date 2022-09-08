c0492d84e51af870a18cbadcdbc3fe26d08a0f62
erc-net <erictaruwinga0827@gmail.com>
2022-09-08 06:55:51 -0700

    erc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** 
+ * main - Entry point
+ *
+ *Return: 0(Success)
+ */
+int main(void)
+{
+  printf("Programming is like building a multilingual puzzle/n");
+    return(0);
+}
