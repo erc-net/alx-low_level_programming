1d5d17448d398649c36f5a7372559e66020bc920
erc-net <111587424+erc-net@users.noreply.github.com>
2022-09-09 21:29:09 +0200

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,43 +1,30 @@
 /*
- *File: 9-print_comb.c
- *Auth: Eric Taruwinga
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- *main - Prints all possible combinations of single-digit numbers.
+ * main - Prints all possible combinations of single-digit numbers.
  *
- *Return: Always 0.
+ * Return: Always 0.
  */
-  int
-main (void)
+int main(void)
 {
+	int num;
 
-int num;
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 
+		putchar(',');
+		putchar(' ');
+	}
 
-for (num = 0; num <= 9; num++)
+	putchar('\n');
 
-    {
-
-putchar ((num % 10) + '0');
-
-if (num == 9)
-
-continue;
-
-
-putchar (',');
-
-putchar (' ');
-
-}
-
-
-putchar ('\n');
-
-
-return (0);
-
+	return (0);
 }
