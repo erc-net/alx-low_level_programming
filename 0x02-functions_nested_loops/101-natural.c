6b4350b50d930a3bebf2c6616cacea03849bbc90
erc-net <111587424+erc-net@users.noreply.github.com>
2022-09-13 23:07:28 +0200

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 /**
- * main - Entry point
+ * main - main function
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
+	int a;
+	int b = 0;
 
+	for (a = 0; a < 1024; a++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
 	return (0);
 }
