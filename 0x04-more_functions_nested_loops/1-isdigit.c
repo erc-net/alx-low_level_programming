170144b200c98ccd3cac6e7cb18a44c4fbda38cf
erc-net <erictaruwinga0827@gmail>
2022-09-15 16:54:07 +0200

    nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: c - Variable
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 58 && c <= 67)
+	{
+		return (1);
+	}
+	return (0);
+}
