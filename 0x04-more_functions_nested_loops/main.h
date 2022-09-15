500777f5dfedd6da43ec789bd5c7c5d7509233fc
erc-net <111587424+erc-net@users.noreply.github.com>
2022-09-15 17:01:27 +0200

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month,int day);
+void print_remaining_days(int month,int day,int year);
+void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +15,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
