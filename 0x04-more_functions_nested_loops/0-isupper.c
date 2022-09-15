c4fa09b6dc8ff1d4f0067a29ad4cb5a9782edb80
erc-net <erictaruwinga0827@gmail>
2022-09-15 16:06:14 +0200

    emma

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
  * _isupper - checks for uppercase character.
- * @: c - Variable
+ * @c: c is a Variable
  * Return: Always 0.
  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		return(1);
+		return (1);
 	}
 	return (0);
 }
