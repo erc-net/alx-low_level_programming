e62b737ba5bc9c8dbef77f6ed14fe0d7d6ff156a
erc-net <erictaruwinga0827@gmail>
2022-09-19 17:14:37 +0200

    lk

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,22 @@
  */
 void rev_string(char *s)
 {
+	int i, j, k, erc;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i -1;
+
+	while (k < j)
+	{
+		erc = s[k];
+		s[k] = s[j];
+		s[j] = erc;
+		k++;
+		j--;
+	}
 }
