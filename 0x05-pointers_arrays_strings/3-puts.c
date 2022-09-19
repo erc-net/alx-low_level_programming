6e39beb22e143d1773289c77b00e3a17b716ce77
erc-net <erictaruwinga0827@gmail>
2022-09-19 16:57:19 +0200

    lk

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,10 +6,11 @@
  */
 void _puts(char *str)
 {
-	for (; *str != '\0';)
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
 	{
-		_putchar(*str);
-		str++;
+		_putchar(str[index]);
 	}
 	_putchar('\n');
 }
