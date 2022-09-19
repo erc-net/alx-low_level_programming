848b123d9032ce8436aa676dfcf82c70eaa4f757
erc-net <111587424+erc-net@users.noreply.github.com>
2022-09-19 17:21:29 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,12 +5,17 @@
  */
 void print_rev(char *s)
 {
-	int index;
+	int len = 0;
 
-	for (index = 0; s[index] != '\0'; index++)
-	for (index = index - 1; s[index] != '\0'; index--)
+	while (*(s + len) != '\0')
 	{
-		_putchar(s[index]);
+		len++;
+	}
+	len--;
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
 	}
 	_putchar('\n');
 }
