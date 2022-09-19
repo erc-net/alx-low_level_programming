208f1bb516310c8293a4f1e519dbdca499bfefae
erc-net <111587424+erc-net@users.noreply.github.com>
2022-09-19 17:29:22 +0200

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,6 +7,17 @@
  */
 int main(void)
 {
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
 
 	return (0);
 }
