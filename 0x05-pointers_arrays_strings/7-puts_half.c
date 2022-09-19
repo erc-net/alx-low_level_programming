31536d209e3cd43ef569733a52b89cf1ea7e1534
erc-net <111587424+erc-net@users.noreply.github.com>
2022-09-19 17:23:43 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,18 @@
  */
 void puts_half(char *str)
 {
-	int i, n, len;
+	int len = 0;
 
-	len = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + len) != '\0')
 		len++;
-
-	n = (len / 2);
-
-	if ((len % 2) == 1)
-		n = ((len + 1) / 2);
-
-	for (i = n; str[i] != '\0'; i++)
-		_putchar(str[i]);
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = len / 2 + 1;
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
 	_putchar('\n');
 }
