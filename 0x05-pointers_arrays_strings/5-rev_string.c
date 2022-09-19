27891e76eb3f5e6b32a11c9ddcd9dbbc801ac8c0
erc-net <erictaruwinga0827@gmail>
2022-09-19 17:08:30 +0200

    lk

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,5 @@
  */
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int fcounter = 0;
-	int i;
 
-	while (s[fcounter] != '\0')
-		fcounter++;
-
-	for (i = 0; i < fcounter; i++)
-	{
-		fcounter--;
-		rev = s[i];
-		s[i] = s[fcounter];
-		s[fcounter] = rev;
-	}
 }
