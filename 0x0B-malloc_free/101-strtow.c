86f4396ca003f8032355d9490ddb172abc36910c
erc-net <111587424+erc-net@users.noreply.github.com>
2022-10-03 19:53:32 +0200

    Update 101-strtow.c
    bk

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,43 +1,81 @@
-#include <stdlib.h>
+
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* **strtow - function that splits a string into words.
-* @str : pointer
-* Return: Char
+* count_word - helper function to count the number of words in a string.
+*
+* @s: string to evaluate
+*
+* Return: number of words
 */
 
+int count_word(char *s)
+{
+int flag, c, w;
+
+flag = 0;
+w = 0;
+
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
+{
+flag = 1;
+w++;
+}
+}
+
+return (w);
+}
+/**
+* **strtow - splits a string into words
+* @str: string to split
+*
+* Return: pointer to an array of strings (Success)
+* or NULL (Error)
+*/
 char **strtow(char *str)
 {
-	char **d;
-	int i;
-	int j = 0;
-	int con = 0;
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
 
-	if (str == NULL)
-		return (NULL);
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
+return (NULL);
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
-			con++;
-	}
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return (NULL);
 
-	d = malloc(sizeof(char) * con);
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (c++ == 0)
+start = i;
+}
 
-	if (d == NULL)
-		return (NULL);
+matrix[k] = NULL;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
-		{
-			*d[j] = str[i];
-			j++;
-		}
-		else
-		{
-		}
-	}
-	return (d);
+return (matrix);
 }
