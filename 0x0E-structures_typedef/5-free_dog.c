ed722a451f370debd9fca1befbc1f65c20c1b8b3
erc-net <111587424+erc-net@users.noreply.github.com>
2022-10-10 21:17:56 +0200

    Update 5-free_dog.c
    Tg

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * free_dog - Entry Point
- * @d: struct to free
- * Return: 0
+ * free_dog -> to free the dog
+ * @d: dog to be freed
  */
+
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
+
+	free(d->owner);
+	free(d->name);
 	free(d);
 }
